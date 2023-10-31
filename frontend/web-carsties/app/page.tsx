f4068123757b0870bf7a2649daf03ef55ed7ffe1
FLITIMouad <64863283+FLITIMouad@users.noreply.github.com>
2023-10-31 16:24:38 +0100

    Add frontend Directory

---
+++ frontend/web-carsties/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h3 className="text-3xl font-semibold">
+        Carsties App!
+      </h3>
+    </>
+  )
+   }
